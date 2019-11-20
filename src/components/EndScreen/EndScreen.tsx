6d21114f157d01a9208bae4afd99db19c1b08b60
Pjaerr <joshcharlesjackson@gmail.com>
2019-11-20 10:55:08 +0000

    Styled the EndScreen component

--- src/components/EndScreen/EndScreen.tsx
+++ src/components/EndScreen/EndScreen.tsx
@@ -15,43 +15,45 @@ type Props = {
 const EndScreen = ({ playerHasWon, statistics, exitFunc }: Props) => {
   return (
     <section className={styles.container}>
-      <h1>You {playerHasWon ? "Win" : "Lose"}!</h1>
-      <p className={styles.conclusionText}>
-        {playerHasWon ? (
-          <>
-            You managed to balance all of your attributes and survived 8 years
-            as Prime Minister
-          </>
-        ) : (
-          <>You failed to keep your nations attributes balanced</>
-        )}
-      </p>
-      <p className={styles.numberOfDecisions}>
-        You made <b>{statistics.numberOfDecisions}</b> decisions
-      </p>
+      <div className={styles.modal}>
+        <h1>You {playerHasWon ? "Win" : "Lose"}!</h1>
+        <p className={styles.conclusionText}>
+          {playerHasWon ? (
+            <>
+              You managed to balance all of your attributes and survived 8 years
+              as Prime Minister
+            </>
+          ) : (
+            <>You failed to keep your nations attributes balanced</>
+          )}
+        </p>
+        <p className={styles.numberOfDecisions}>
+          You made <b>{statistics.numberOfDecisions}</b> decisions
+        </p>
 
-      <ul className={styles.attributes}>
-        <li>
-          <b>Financial</b>: {statistics.attributes.financial}
-        </li>
+        <ul className={styles.attributes}>
+          <li>
+            <b>Financial</b>: {statistics.attributes.financial}
+          </li>
 
-        <li>
-          <b>Population Happiness</b>:{" "}
-          {statistics.attributes.populationHappiness}
-        </li>
+          <li>
+            <b>Population Happiness</b>:{" "}
+            {statistics.attributes.populationHappiness}
+          </li>
 
-        <li>
-          <b>Domestic Political Favour</b>:{" "}
-          {statistics.attributes.domesticPoliticalFavour}
-        </li>
-        <li>
-          <b>Foreign Political Favour</b>:{" "}
-          {statistics.attributes.foreignPoliticalFavour}
-        </li>
-      </ul>
-      <button className={styles.exitButton} onClick={() => exitFunc()}>
-        Exit
-      </button>
+          <li>
+            <b>Domestic Political Favour</b>:{" "}
+            {statistics.attributes.domesticPoliticalFavour}
+          </li>
+          <li>
+            <b>Foreign Political Favour</b>:{" "}
+            {statistics.attributes.foreignPoliticalFavour}
+          </li>
+        </ul>
+        <button className={styles.exitButton} onClick={() => exitFunc()}>
+          Exit
+        </button>
+      </div>
     </section>
   );
 };
