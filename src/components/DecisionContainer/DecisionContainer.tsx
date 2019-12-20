59f4700327a430cd72e26bfe1421eb02be8762c9
Pjaerr <joshcharlesjackson@gmail.com>
2019-12-20 12:53:02 +0000

    Documented Architectural Approach

--- src/components/DecisionContainer/DecisionContainer.tsx
+++ src/components/DecisionContainer/DecisionContainer.tsx
@@ -3,7 +3,15 @@ import React from "react";
 import { IDecision } from "../../interfaces/IDecision";
 import Decision from "../Decision/Decision";
 import IAttributes from "../../interfaces/IAttributes";
-
+/**
+ * TODO:
+ * Where a component is heavily tied to another part of the application
+ * like below through the use of an identical nextTurn function,
+ * try and abstract this away so that they both sort of inherit/implement
+ * a typescript class/interface so it is clear that they are both linked and
+ * cannot really be used as a pure UI component.
+ *
+ */
 type Props = {
   decision: IDecision;
   nextTurn: (attributeAdjustments: IAttributes) => void;
