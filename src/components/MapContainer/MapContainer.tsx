e2652325b888acb4ae2ab028954797d83bf64000
Pjaerr <joshcharlesjackson@gmail.com>
2020-02-11 16:31:59 +0000

    Made the SVG pannable and zoomable on desktop
    Experimented for ages and finally got the SVG map to be pannable and zoomable (via dragging and scrolling) on desktop. Made this into a component called PannableSVG that must have an SVG child nested inside of it, it will then clone that child and get a ref to it, returning the child so the SVG renders as normal but then using the ref it has obtained to add some event listeners, firstly it adds an onmousedown event listener which will enable dragging and store the mouse position when it is triggered, then it uses an onmousemove listener and, if the draggig is enabled it will update the SVGs viewBox by the movement values (varying by speed). Similar process for zooming but using the mousewheel event. Currently works as expected, although panning could be slightly smoother. Need to now repeat same functionality but for the the touch events

--- src/components/MapContainer/MapContainer.tsx
+++ src/components/MapContainer/MapContainer.tsx
@@ -1,6 +1,7 @@
-import React, { useEffect, useState } from "react";
+import React from "react";
 
 //Components
+import PannableSVG from "../PannableSVG/PannableSVG";
 import MapProvince from "../MapProvince/MapProvince";
 
 //Styles
@@ -14,79 +15,9 @@ type Props = {
 };
 
 const MapContainer = ({ onProvinceClick, provinces }: Props) => {
-  const svgRef = React.createRef<SVGSVGElement>();
-  const groupRef = React.createRef<SVGGElement>();
-
-  const [width, setWidth] = useState(0);
-  const [height, setHeight] = useState(0);
-  const [pos, setPos] = useState({ x: 0, y: 0 });
-
-  let isDragActive = false;
-
-  useEffect(() => {
-    if (svgRef.current) {
-      const svgBoundingBox = svgRef.current.getBBox();
-      setWidth(svgBoundingBox.x + svgBoundingBox.width + svgBoundingBox.x);
-      setHeight(svgBoundingBox.y + svgBoundingBox.height + svgBoundingBox.y);
-
-      let currentMousePos: { x: number; y: number };
-
-      svgRef.current.addEventListener("mousedown", e => {
-        currentMousePos = {
-          x: e.clientX,
-          y: e.clientY
-        };
-
-        isDragActive = true;
-      });
-      svgRef.current.addEventListener("mouseup", e => {
-        isDragActive = false;
-      });
-      svgRef.current.addEventListener("mousemove", e => {
-        if (isDragActive) {
-          let distanceToMove = { x: 0, y: 0 };
-
-          // if (e.clientX < currentMousePos.x) {
-          //   //move left
-          //   distanceToMove.x -= 2;
-          // }
-          // if (e.clientX > currentMousePos.x) {
-          //   //move right
-          //   distanceToMove.x += 2;
-          // }
-
-          // if (e.clientY < currentMousePos.y) {
-          //   //move down
-          //   distanceToMove.y -= 2;
-          // }
-          // if (e.clientY > currentMousePos.y) {
-          //   //move up
-          //   distanceToMove.y += 2;
-          // }
-
-          distanceToMove.x = (currentMousePos.x - e.clientX) * -0.05;
-          distanceToMove.y = (currentMousePos.y - e.clientY) * -0.05;
-
-          if (groupRef.current) {
-            pos.x += distanceToMove.x;
-            pos.y += distanceToMove.y;
-            groupRef.current.setAttribute(
-              "transform",
-              `translate(${pos.x}, ${pos.y})`
-            );
-          }
-        }
-      });
-    }
-  }, [svgRef]);
-
   return (
-    <svg
-      className={styles.container}
-      ref={svgRef}
-      viewBox={`0 0 ${width} ${height}`}
-    >
-      <g ref={groupRef}>
+    <PannableSVG zoomSpeed={1}>
+      <svg className={styles.container}>
         {provinces.map(province => (
           <MapProvince
             key={province.name}
@@ -97,8 +28,8 @@ const MapContainer = ({ onProvinceClick, provinces }: Props) => {
             }}
           />
         ))}
-      </g>
-    </svg>
+      </svg>
+    </PannableSVG>
   );
 };
 
