b5bdea6bd8d6aae6e7f0f924bcb7f799eacbb98d
Pjaerr <joshcharlesjackson@gmail.com>
2020-02-27 14:20:11 +0000

    Added a bunch more tests using React-Testing-Library

---
+++ src/components/MapProvinceInfo/MapProvinceInfo.test.tsx
@@ -0,0 +1,32 @@
+import "@testing-library/jest-dom";
+import React from "react";
+import { render, fireEvent, screen } from "@testing-library/react";
+import MapProvinceInfo from "./MapProvinceInfo";
+import IProvince from "../../interfaces/IProvince";
+
+test("Calls onCloseFunc() prop when the close button is clicked", () => {
+  //Useless province object to avoid missing prop errors when testing MapProvinceInfo component
+  const mockProvince: IProvince = {
+    factors: {
+      averageSalary: 0,
+      nonWhiteBritishEthnicPercentage: 0,
+      numberOfUniversities: 0,
+      populationDensity: 0
+    },
+    happiness: 100,
+    isInParty: false,
+    name: "Mock Province",
+    population: 10000
+  };
+
+  let count = 5;
+
+  render(
+    <MapProvinceInfo onCloseFunc={() => (count += 5)} province={mockProvince} />
+  );
+
+  const closeButton = screen.getByAltText(/Back Arrow Icon/i);
+  fireEvent.click(closeButton);
+
+  expect(count).toBe(10);
+});
