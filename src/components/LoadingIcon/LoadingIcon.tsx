8cdc0061a58117ecb18c1609872ab0ab18dc2976
Pjaerr <joshcharlesjackson@gmail.com>
2020-05-05 16:13:42 +0100

    Pushing backend files for deploy

--- src/components/LoadingIcon/LoadingIcon.tsx
+++
@@ -1,9 +0,0 @@
-import React from "react";
-
-import styles from "./LoadingIcon.module.scss";
-
-const LoadingIcon = () => {
-  return <div className={styles.loadingIcon}></div>;
-};
-
-export default LoadingIcon;
--- src/interfaces/IAttributes.ts
+++
@@ -1,8 +0,0 @@
-interface IAttributes {
-  financial: number;
-  populationHappiness: number;
-  domesticPoliticalFavour: number;
-  foreignPoliticalFavour: number;
-}
-
-export default IAttributes;
